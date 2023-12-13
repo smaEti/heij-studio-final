223c5ee64cad6ab2cf61154e0e7243315cb1bcef
Behrad Tabatabaee <behradtabatabaee3@gmail.com>
2023-12-13 20:51:27 +0330

    Hero Section Finished

---
+++ components/desktopNav.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+export default function DesktopNav() {
+	return (
+		<div className="hidden md:block md:absolute top-0 w-full px-20 py-5">
+			<div className="flex justify-between">
+				<div>
+					<img
+						src="/logowhite.png"
+						alt="logo"
+						className="md:w-20 lg:w-20 xl:w-24 2xl:w-28"
+					/>
+				</div>
+				<div className="flex gap-5">
+					<Link
+						href="#"
+						className="bg-[#ff5900]/75 text-white flex items-center justify-center my-2 h-12 w-24 lg:w-auto lg:h-fit md:text-xs lg:px-6 lg:py-3 xl:px-8 xl:py-4 2xl:px-12 2xl:py-5 lg:my-3 xl:my-4 2xl:my-5 rounded-2xl font-semibold font-outfit xl:text-base 2xl:text-xl hover:bg-white hover:text-black transition-all ease-in-out duration-300"
+					>
+						Projects
+					</Link>
+					<Link
+						href="#"
+						className="bg-[#ff5900]/75 text-white flex items-center justify-center my-2 h-12 w-24 lg:w-auto lg:h-fit md:text-xs lg:px-6 lg:py-3 xl:px-8 xl:py-4 2xl:px-12 2xl:py-5 lg:my-3 xl:my-4 2xl:my-5 rounded-2xl font-semibold font-outfit xl:text-base 2xl:text-xl hover:bg-white hover:text-black transition-all ease-in-out duration-300"
+					>
+						About Us
+					</Link>
+					<Link
+						href="#"
+						className="bg-[#ff5900]/75 text-white flex items-center justify-center my-2 h-12 w-24 lg:w-auto lg:h-fit md:text-xs lg:px-6 lg:py-3 xl:px-8 xl:py-4 2xl:px-12 2xl:py-5 lg:my-3 xl:my-4 2xl:my-5 rounded-2xl font-semibold font-outfit xl:text-base 2xl:text-xl hover:bg-white hover:text-black transition-all ease-in-out duration-300"
+					>
+						Contact Us
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ components/hero.jsx
@@ -0,0 +1,19 @@
+import DesktopNav from "./desktopNav";
+import MobileNav from "./mobileNav";
+export default function Hero() {
+	return (
+		<div className="w-screen">
+			<div className="h-[50vh] sm:h-screen relative">
+				<video
+					src="/Demoreel.mp4"
+					autoPlay
+					loop
+					muted
+					className="object-cover h-full w-full"
+				></video>
+			</div>
+            <DesktopNav/>
+            <MobileNav/>
+		</div>
+	);
+}
