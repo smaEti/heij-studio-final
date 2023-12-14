600db3a4d158e7894f046ddb42af2d98fea0cd85
Amin Ehterami <A.Ehterami@proton.me>
2023-12-14 23:11:14 +0330

    contact us completed (almost)

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <section className="md:h-screen flex items-center justify-center px-5">
-      <div className="md:container bg-background  rounded-2xl flex flex-col-reverse md:flex-row justify-between gap-10 p-3 md:p-8 mt-20">
+      <div className="md:container bg-background  rounded-2xl flex flex-col-reverse md:flex-row justify-between gap-10 p-3 md:p-8 my-20">
         <div className="flex flex-col basis-6/12 md:py-12 md:gap-12 gap-3 md:mb-0 mb-6 self-center">
           <h1 className="font-extrabold text-4xl md:text-6xl xl:text-8xl 2xl:text-9xl font-serif md:pl-2 text-primary">
             About Us
--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,10 +1,35 @@
-export default function Contact(){
-    return (<section className="h-screen flex justify-center items-center px-5">
-        <div className="bg-background md:h-[70vh] rounded-2xl flex justify-around container p-8">
-            <div className="basis-3/6">
-                <h1 className="font-extrabold text-4xl md:text-8xl font-serif md:pl-2 text-primary">Contact Us</h1>
-            </div>
-            <div className="basis-2/6">sadas</div>
+export default function Contact() {
+  return (
+    <section className="md:h-screen flex justify-center items-center px-5 my-20">
+      <div className="bg-background md:h-[70vh] rounded-2xl flex flex-col md:flex-row md:justify-around md:container p-8 gap-5">
+        <div className="basis-3/6 flex flex-col items-start">
+          <img src="/logo.png" alt="logo" className="h-3/4 w-1/2 self-center" />
+          <div className="basis-1/2 flex flex-col gap-5 ">
+            {/* <h1 className="font-extrabold text-4xl md:text-9xl font-serif text-primary">Contact Us</h1> */}
+            <p className="text-primary font-outfit text-2xl md:text-4xl xl:text-5xl">
+              Contact us today and let's make something amazing together.
+            </p>
+            {/* <p>asdasd</p>
+                <p>Email : Heijstudio@gmail.com</p> */}
+          </div>
         </div>
-    </section>);
+        <div className="basis-2/6 flex flex-col gap-3 justify-center text-primary">
+          <h2>Email</h2>
+          <p>Heijstudio@gmail.com</p>
+          <hr className="bg-primary h-1" />
+          <h2>Telegram</h2>
+          <p>+98-9355525896</p>
+          <hr className="bg-primary h-1" />
+          <h2>Behance</h2>
+          <p>some address</p>
+          <hr className="bg-primary h-1" />
+          <h2>Linkedin</h2>
+          <p>some address</p>
+          <hr className="bg-primary h-1" />
+          <h2>Instagram</h2>
+          <p>some address</p>
+        </div>
+      </div>
+    </section>
+  );
 }
