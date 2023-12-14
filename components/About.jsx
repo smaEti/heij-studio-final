f84f6231ae36f6d02f486ee6ecc32b610dc54319
Amin Ehterami <A.Ehterami@proton.me>
2023-12-14 23:25:01 +0330

    links on the main page changed

--- components/About.jsx
+++ components/About.jsx
@@ -2,11 +2,11 @@ export default function About() {
   return (
     <section className="md:h-screen flex items-center justify-center px-5">
       <div className="md:container bg-background  rounded-2xl flex flex-col-reverse md:flex-row justify-between gap-10 p-3 md:p-8 my-20">
-        <div className="flex flex-col basis-6/12 md:py-12 md:gap-12 gap-3 md:mb-0 mb-6 self-center">
+        <div className="flex flex-col basis-5/12 md:py-12 md:gap-12 gap-3 md:mb-0 mb-6 self-center">
           <h1 className="font-extrabold text-4xl md:text-6xl xl:text-8xl 2xl:text-9xl font-serif md:pl-2 text-primary">
             About Us
           </h1>
-          <div className="text-primary font-outfit text-2xl md:text-4xl xl:text-5xl leading-none text-justify md:pl-2">
+          <div className="text-primary font-outfit text-2xl md:text-xl xl:text-4xl leading-none text-justify md:pl-2">
             <p>
               We are HEIJ Studio , a creative team of motion designers,
               animatiors, and storytellers. We help you bring your ideas to life
--- components/desktopNav.jsx
+++ components/desktopNav.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default function DesktopNav() {
 	return (
-		<div className="hidden md:block md:absolute top-0 w-full px-20 py-8">
+		<div className="hidden md:block md:absolute top-0 w-full px-20 py-20">
 			<div className="flex justify-between items-start">
 				<div>
 					<img
@@ -10,22 +10,22 @@ export default function DesktopNav() {
 						className="md:w-16 lg:w-16 xl:w-20 2xl:w-20"
 					/>
 				</div>
-				<div className="flex gap-5">
+				<div className="flex gap-5 items-center justify-between">
 					<Link
 						href="#"
-						className="bg-[#ff5900]/75 text-white flex items-center justify-center my-2 h-12 w-24 lg:w-auto lg:h-fit md:text-xs lg:px-6 lg:py-3 xl:px-8 xl:py-4 2xl:px-12 2xl:py-5 lg:my-3 xl:my-4 2xl:my-5 rounded-2xl font-semibold font-outfit xl:text-base 2xl:text-xl hover:bg-white hover:text-black transition-all ease-in-out duration-300"
+						className=" text-white w-24 lg:w-auto lg:h-fit md:text-xs font-semibold font-serif xl:text-base 2xl:text-xl underline underline-offset-8"
 					>
 						Projects
 					</Link>
 					<Link
 						href="#"
-						className="bg-[#ff5900]/75 text-white flex items-center justify-center my-2 h-12 w-24 lg:w-auto lg:h-fit md:text-xs lg:px-6 lg:py-3 xl:px-8 xl:py-4 2xl:px-12 2xl:py-5 lg:my-3 xl:my-4 2xl:my-5 rounded-2xl font-semibold font-outfit xl:text-base 2xl:text-xl hover:bg-white hover:text-black transition-all ease-in-out duration-300"
+						className="text-white w-24 lg:w-auto lg:h-fit md:text-xs font-semibold font-serif xl:text-base 2xl:text-xl underline underline-offset-8"
 					>
 						About Us
 					</Link>
 					<Link
 						href="#"
-						className="bg-[#ff5900]/75 text-white flex items-center justify-center my-2 h-12 w-24 lg:w-auto lg:h-fit md:text-xs lg:px-6 lg:py-3 xl:px-8 xl:py-4 2xl:px-12 2xl:py-5 lg:my-3 xl:my-4 2xl:my-5 rounded-2xl font-semibold font-outfit xl:text-base 2xl:text-xl hover:bg-white hover:text-black transition-all ease-in-out duration-300"
+						className="text-white w-24 lg:w-auto lg:h-fit md:text-xs font-semibold font-serif xl:text-base 2xl:text-xl underline underline-offset-8"
 					>
 						Contact Us
 					</Link>
