105f4039f0b78517058dbaea67e3aa9523145351
Amin Ehterami <A.Ehterami@proton.me>
2023-12-20 12:56:14 +0330

    contact form

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -9,30 +9,63 @@ export default function ContactForm() {
     subject: "",
     message: "",
   });
+  const [error, setError] = useState(null);
   return (
     <>
+      {error && (
+        <div className="p-2 rounded-lg bg-red-500 text-background">{error}</div>
+      )}
       <label>Your name *: </label>
-      <Input type="text"  onChange={(e) => {
-        setData({...data , name : e.target.value})
-      }}/>
+      <Input
+        type="text"
+        onChange={(e) => {
+          setData({ ...data, name: e.target.value });
+        }}
+        value={data.name}
+      />
       <label>Email *: </label>
-      <Input type="text" onChange={(e) => {
-        setData({...data , email : e.target.value})
-      }}/>
+      <Input
+        type="text"
+        onChange={(e) => {
+          setData({ ...data, email: e.target.value });
+        }}
+        value={data.email}
+      />
       <label>Subject *: </label>
-      <Input type="text" onChange={(e) => {
-        setData({...data , subject : e.target.value})
-      }}/>
+      <Input
+        type="text"
+        onChange={(e) => {
+          setData({ ...data, subject: e.target.value });
+        }}
+        value={data.subject}
+      />
       <label>Your message *:</label>
-      <Input type="text" onChange={(e) => {
-        setData({...data , message : e.target.value})
-      }}/>
-      <button className="bg-secondry text-background p-2 rounded-lg hover:bg-orange-500" onClick={sendData({data})}>Submit</button>
+      <Input
+        type="text"
+        onChange={(e) => {
+          setData({ ...data, message: e.target.value });
+        }}
+        value={data.message}
+      />
+      <button
+        className="bg-secondry text-background p-2 rounded-lg hover:bg-orange-500"
+        onClick={() => {
+          sendData({ data, setError });
+        }}
+      >
+        Submit
+      </button>
     </>
   );
 }
 
-
-async function sendData({props}){
-  return;
+async function sendData({ data, setError }) {
+  const response = await fetch(`/api/contactrequest/create`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  const backeddata = await response.json();
+  setError(backeddata.message);
+  return backeddata;
 }
--- pages/api/contactrequest/create.js
+++ pages/api/contactrequest/create.js
@@ -1,49 +1,49 @@
 import { db } from "@/prisma/client";
 import vine from "@vinejs/vine";
 export default async function handler(req, res) {
-	if (req.method === "POST") {
-		try {
-			const { name, email, subject, message } = req.body;
-			const schema = vine.object({
-                name: vine.string(),
-                email: vine.string().email(),
-                subject: vine.string(),
-                message: vine.string()
-			});
-			const data = {
-				name: name,
-				email: email,
-				subject: subject,
-                message: message
-			};
-			const array = [email, name, comment];
-			let errorsCount = 0;
-			array.map((item) => {
-				if (item == "" || item == undefined || item == null) {
-					errorsCount += 1;
-				}
-			});
-			const output = await vine.validate({
-				schema,
-				data,
-			});
-			if (errorsCount > 0) {
-				res.status(422).send({ message: "fields are required!" });
-				errorsCount = 0;
-			} else {
-				const result = await db.contactrequest.create({
-					data: {
-						name: name,
-						email: email,
-						subject: subject,
-                        message: message,
-						is_seen: false,
-					},
-				});
-				res.status(200).send(result);
-			}
-		} catch (error) {
-			return res.status(500).json({ message: error.message });
-		}
-	}
+  if (req.method === "POST") {
+    try {
+      const { name, email, subject, message } = req.body;
+      const schema = vine.object({
+        name: vine.string(),
+        email: vine.string().email(),
+        subject: vine.string(),
+        message: vine.string(),
+      });
+      const data = {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message,
+      };
+      const array = [email, name, subject, message];
+      let errorsCount = 0;
+      array.map((item) => {
+        if (item == "" || item == undefined || item == null) {
+          errorsCount += 1;
+        }
+      });
+      const output = await vine.validate({
+        schema,
+        data,
+      });
+      if (errorsCount > 0) {
+        res.status(422).send({ message: "fields are required!" });
+        errorsCount = 0;
+      } else {
+        const result = await db.contactrequest.create({
+          data: {
+            name: name,
+            email: email,
+            subject: subject,
+            message: message,
+            is_seen: false,
+          },
+        });
+        res.status(200).send(result);
+      }
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
 }
