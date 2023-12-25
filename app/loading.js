115edc9d834ccf74d894b60a4b7b4112972f19e3
Amin Ehterami <A.Ehterami@proton.me>
2023-12-25 21:30:17 +0330

    loading and somework for projects

--- app/loading.js
+++ app/loading.js
@@ -1,11 +1,10 @@
 export default function Loading() {
-    // Or a custom loading skeleton component
-    return (<section className="flex justify-center items-center">
-        <img
-            src="/logo.png"
-            alt="logo"
-            className="animate-bounce"
-          />
-          <h1 className="lg:text-8xl text-6xl">Loading . . .</h1>
-    </section>)
-  }
+  return (
+    <section className="flex flex-col justify-center items-center h-screen">
+      <div className="lg:w-1/6 md:w-1/5 w-1/2 h-full flex flex-col justify-center items-center">
+      <img src="/logo.png" alt="logo" className="animate-bounce w-1/2" />
+      <h1 className="lg:text-6xl text-5xl text-background font-Aleo">Loading...</h1>
+      </div>
+    </section>
+  );
+}
---
+++ app/test/page.js
@@ -0,0 +1,21 @@
+export default async function Loading() {
+    return (
+<div class="md:container grid grid-cols-3 gap-4">
+  <div className="bg-secondry rounded-xl h-[30vh]">01</div>
+  <div className="bg-secondry rounded-xl h-[30vh]">01</div>
+  <div className="bg-secondry rounded-xl h-[30vh]">01</div>
+  <div className="bg-secondry rounded-xl h-[30vh]">01</div>
+  <div className="bg-secondry rounded-xl h-[30vh]">01</div>
+  <div className="bg-secondry rounded-xl h-[30vh]">01</div>
+  <div className="bg-secondry rounded-xl h-[30vh]">01</div>
+  <div className="bg-secondry rounded-xl h-[30vh]">01</div>
+  <div className="bg-secondry rounded-xl h-[30vh]">09</div>
+  <div className="bg-secondry rounded-xl h-[30vh]">01</div>
+  <div className="bg-secondry rounded-xl h-[30vh]">01</div>
+  <div className="bg-secondry rounded-xl h-[30vh]">01</div>
+  <div className="bg-secondry rounded-xl h-[30vh]">01</div>
+  <div className="bg-secondry rounded-xl h-[30vh]">09</div>
+</div>
+    );
+  }
+  
